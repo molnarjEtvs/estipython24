e1b2cd71ab30f00e6122ab65a68e5748dd986b3b
Molnár Tamás <molnarj@EOTVOS2.LOCAL>
2024-03-19 18:01:42 +0100

    d

--- esti24Webprogramozas/20240318/ertekelos/script.js
+++ esti24Webprogramozas/20240318/ertekelos/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $(".csillag").mouseover(function(){
         let ig = $(this).attr("sorszam");
         $(".csillag").removeClass("halvanyCsillag");
@@ -19,5 +18,26 @@ $(document).ready(function(){
             $("#csillag"+i).addClass("sotetCsillag");
         }
     });
+
+
+    $("#ertekelBtn").click(function(){
+        let csillagokSzama = 1;
+        let szoveg = "VALAMIT";
+        let template = '<div class="egyErtkeles">';
+        template += '<div class="csillagBlock">';
+        for(let i=1;i<=csillagokSzama;i++){
+            template += '<span class="jobbCsillagTeli">&starf;</span>';
+        }
+        template += '</div>';
+
+        template += '<div class="szovegesErtekeles">';
+        template += szoveg;
+        template += '</div>';
+
+        template += '</div>';
+
+        $("#hozzaAdottErtekelesek").append(template);
+    });
+
 
 });
